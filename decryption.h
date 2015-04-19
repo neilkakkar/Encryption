f6d474e658d408616a135afff937fddf986ae126
Neil Kakkar <neilkakkar@gmail.com>
2015-04-19 21:14:30 +0530

    Create decryption.h

---
+++ decryption.h
@@ -0,0 +1,20 @@
+#include <iostream> 	// Allows input output operations
+#include <iomanip>
+#include <math.h>
+#include <cstdio>
+
+#include "matrix.h"
+
+/*******************************************************************************
+* Function Name  : inv_array_map
+* Description    : Maps inv_mapped_array to decrypt_plain_text_zero_padded using map array
+*******************************************************************************/
+
+void inv_array_map(char inv_mapped_array[ARRAY_SIZE], char decrypt_plain_text_zero_padded[ARRAY_SIZE]);
+
+/*******************************************************************************
+* Function Name  : keygen
+* Description    : Generates key used for encryption
+*******************************************************************************/
+
+//void keygen(float key[N][N], int marker);
